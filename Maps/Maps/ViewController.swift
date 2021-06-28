19cc9c59f58717d5f3b9806ab448b620d4ecd24d
Payal <Payal>
2021-06-28 15:07:59 +0530

    dding fuke

--- Maps/Maps/ViewController.swift
+++ Maps/Maps/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var map:MKMapView!
+    var locationManager:CLLocationManager?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.allowsBackgroundLocationUpdates = true
+        locationManager?.requestLocation()
+        
+        map.showsUserLocation = true
+        map.delegate = self
+        
     }
 
 
+}
+
+extension ViewController:MKMapViewDelegate{
+    
+}
+
+extension ViewController:CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
 }
 
